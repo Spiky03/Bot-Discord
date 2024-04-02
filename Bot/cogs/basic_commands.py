967a6d77e50929b0f0c846059b5728598526915d
cichester <115873556+cichester@users.noreply.github.com>
2024-04-02 18:22:50 +0200

    Update basic_commands.py

--- Bot/cogs/basic_commands.py
+++ Bot/cogs/basic_commands.py
@@ -1,62 +1,62 @@
-import bot_libraries.py as bl
+import bot_libraries.py
 
-class basic_commands(bl.commands.Cog):
+class basic_commands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
     
     #### JOIN-ROLE
-    bl.@commands.Cog.listener()
+    @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.bl.guild.id != 809055669075312691:
+        if member.guild.id != 809055669075312691:
             return
         
         if member.bot:
-            role = discord.bl.utils.get(member.guild.roles, name="BOT")
+            role = discord.utils.get(member.guild.roles, name="BOT")
         else:
-            role = discord.bl.utils.get(member.guild.roles, name="Disperso")
+            role = discord.utils.get(member.guild.roles, name="Disperso")
         await member.add_roles(role)
 
     # AUTO REACTIONS
-    bl.@commands.Cog.listener()
+    @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.bl.channel.id in [1214564510833319988, 1202550269057957938]:
-            await message.bl.add_reaction('✅')
-            await message.bl.add_reaction('❌')
+        if message.channel.id in [1214564510833319988, 1202550269057957938]:
+            await message.add_reaction('✅')
+            await message.add_reaction('❌')
     
     # CLEAR
-    bl.@commands.hybrid_command(name="clear", description="Cancella più messaggi assieme")
-    bl.@commands.has_permissions(manage_messages=True)
-    bl.@commands.bot_has_permissions(manage_messages=True)
+    @commands.hybrid_command(name="clear", description="Cancella più messaggi assieme")
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
     async def clear(self, ctx, amount: int):
-        await ctx.bl.channel.purge(limit=amount)
+        await ctx.channel.purge(limit=amount)
         if amount == 1:
-            await ctx.bl.send(f"1 Messaggio eliminato", delete_after=5)
+            await ctx.send(f"1 Messaggio eliminato", delete_after=5)
         else:
-            await ctx.bl.send(f"{amount} Messaggi eliminati", delete_after=5)
+            await ctx.send(f"{amount} Messaggi eliminati", delete_after=5)
 
-    bl.@clear.error
+    @clear.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
-            await ctx.bl.send("Mi dispiace, ma non hai i permessi per usare questo comando.", delete_after=5)
+            await ctx.send("Mi dispiace, ma non hai i permessi per usare questo comando.", delete_after=5)
         elif isinstance(error, commands.BotMissingPermissions):
-            await ctx.bl.send("Mi dispiace, ma il bot non ha i permessi per eseguire questo comando.", delete_after=5)
+            await ctx.send("Mi dispiace, ma il bot non ha i permessi per eseguire questo comando.", delete_after=5)
 
     # TEST
-    bl.@commands.command()
+    @commands.command()
     async def test(self, ctx):
-        await ctx.bl.channel.purge(limit=1)
-        await ctx.bl.send(ctx.message.content[5:])
+        await ctx.channel.purge(limit=1)
+        await ctx.send(ctx.message.content[5:])
         for attachment in ctx.message.attachments:
-            await ctx.bl.send(attachment.url)
+            await ctx.send(attachment.url)
 
     # SHUTDOWN
-    bl.@commands.command()
-    bl.@commands.is_owner()
+    @commands.command()
+    @commands.is_owner()
     async def shutdown(self, ctx):
-        await ctx.bl.send(content = "Spegnendo il bot...", delete_after=3)
-        await asyncio.bl.sleep(3)
-        await self.bl.bot.close()
+        await ctx.send(content = "Spegnendo il bot...", delete_after=3)
+        await asyncio.sleep(3)
+        await self.bot.close()
 
 async def setup(bot):
-    await bot.bl.add_cog(basic_commands(bot))
+    await bot.add_cog(basic_commands(bot))
