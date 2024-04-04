f1eba45b115351f07d11f08ac99747e7cea8263c
Spiky03 <62434536+Spiky03@users.noreply.github.com>
2024-04-04 12:33:13 +0200

    Update main.py

--- Bot/main.py
+++ Bot/main.py
@@ -1,24 +1,30 @@
-import bot_libraries.py
+import discord
+import asyncio
+from discord.ext import commands
+from discord import app_commands
+
+import os
+from dotenv import load_dotenv
 load_dotenv()
 
 # TOKEN
-TOKEN = os.getenv("PROVA")
+TOKEN = os.getenv("TOKEN")
 
 # PREFIX AND INTENTS
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='^', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 @bot.command(name="sync")
 async def sync(ctx):
     synced = await bot.tree.sync()
-    print(f"Synced {len(synced)}    command(s).")
+    print(f"Synced {len(synced)} command(s).")
 
 # READY
 @bot.event  
 async def on_ready():
     
     # COGS
-    for file in os.listdir("C:/Users/gagli/OneDrive/Documenti/Bot-Discord/Bot/cogs"):
+    for file in os.listdir("/home/container/cogs"):
         if file.endswith(".py"):
             await bot.load_extension(f"cogs.{file[:-3]}")
 
